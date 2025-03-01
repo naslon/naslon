6f43898e55dadaf50e2c533422ae305ee872479a
Naslon <155392065+naslon@users.noreply.github.com>
2025-03-01 03:03:17 -0400

    Lançamento Site Oficial Naslon
    01/03/2025

---
+++ script.js
@@ -0,0 +1,48 @@
+function executaAcao0(){
+	window.location = "index.html";
+}
+function executaAcao1(){
+	window.location = "gabe.html";
+}
+function executaAcao2(){
+	window.location = "wept.html";
+}
+function executaAcao3(){
+	window.location = "nanotecnologia.html";
+}
+function executaAcao4(){
+	window.location = "futuro.html";
+}
+function executaAcao5(){
+	window.location = "expansao.html";
+}
+function executaAcao6(){
+	window.location = "sobre.html";
+}
+
+/* Barra de pesquisa */
+
+function lupa(){
+  document.getElementById('pesquisa').style.display="inline-block";
+}
+function esconder(){
+   document.getElementById('pesquisa').style.display="none";
+}
+
+/* Redes Sociais */
+
+function youtube(){
+	window.open(`https://www.youtube.com/channel/UCXc5HrGNsktwMKcwFuLgkjg`);
+}
+
+function instagram(){
+	window.open(`https://www.instagram.com/_naslon_`);
+}
+
+function twitter(){
+	window.open(`https://x.com/_naslon_`);
+}
+
+function threads(){
+	window.open(`https://www.threads.net/@_naslon_`);
+}
